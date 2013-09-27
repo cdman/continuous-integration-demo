c7dbe029602180184d7b601d12ec8a9f90b9a57b
Attila-Mihaly Balazs <x_at_y_or_z@yahoo.com>
2013-09-27 17:50:48 +0300

    Added buildscripts

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-C:\Users\cdman\Documents\nuget.exe install CDDemo\packages.config -OutputDirectory packages
+"C:\Program Files\NuGet\nuget.exe" install CDDemo\packages.config -OutputDirectory packages
 rmdir /S /Q CDDemo\bin
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe CDDemo.sln /p:Configuration=Debug
